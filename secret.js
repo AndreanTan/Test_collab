1cb3b8d6a22db85f3400c40a50f3ae7fb84815ce
Arief Rubani <hidayat.arief44@yahoo.com>
2023-07-17 14:16:16 +0700

    penting guys

--- secret.js
+++ secret.js
@@ -1,3 +1,6 @@
 console.log("rahasia");
 console.log("rahasia 2");
 console.log("fernando <3 mamat")
+
+
+console.log(" guys pas pulang jangan pulang langsung ya, kita ketemu sebentar diluar purwa, ada yang mau gue discuss")
