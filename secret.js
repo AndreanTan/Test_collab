bd6d24ae476648f61812f7461eeb3b77fdc1776d
Arief Rubani <hidayat.arief44@yahoo.com>
2023-07-17 14:18:37 +0700

    update

--- secret.js
+++ secret.js
@@ -4,3 +4,4 @@ console.log("fernando <3 mamat")
 
 
 console.log(" guys pas pulang jangan pulang langsung ya, kita ketemu sebentar diluar purwa, ada yang mau gue discuss")
+console.log(" sebenetar aja kok ya")
